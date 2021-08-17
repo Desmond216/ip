ec76d922c01a48c1b529be4ec2d8a05bc08e55c5
Desmond216 <zhenjiesoh@gmail.com>
2021-08-18 00:33:47 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello I'm Duke\n What can I do for u");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        while (input.equals("bye") == false) {
+            System.out.println(input);
+            input = br.readLine();
+
+
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
