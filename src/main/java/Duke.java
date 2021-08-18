a22285f3fd17ceb75628be7e0bf51901a72b8a67
Desmond216 <zhenjiesoh@gmail.com>
2021-08-19 00:24:57 +0800

    added level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,13 +12,23 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello I'm Duke\n What can I do for u");
+        System.out.println("--------------------------");
+        ArrayList<String> lst = new ArrayList<String>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         while (input.equals("bye") == false) {
-            System.out.println(input);
+            if(input.equals("list") == false) {
+                System.out.println("added: " + input);
+                lst.add(input);
+                System.out.println("--------------------------");
+                input = br.readLine();
+                continue;
+            }
+            for(int i = 0; i < lst.size(); i++) {
+                System.out.println(i+1 + ": " + lst.get(i));
+            }
+            System.out.println("--------------------------");
             input = br.readLine();
-
-
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
